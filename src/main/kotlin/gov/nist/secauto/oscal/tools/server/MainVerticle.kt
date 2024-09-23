c5c08069026c60882e378713c1edbcdf98d3c978
wandmagic <156969148+wandmagic@users.noreply.github.com>
2024-09-23 18:02:34 +0000

    impliment oscal-cli command server

--- src/main/kotlin/gov/nist/secauto/oscal/tools/server/MainVerticle.kt
+++ src/main/kotlin/gov/nist/secauto/oscal/tools/server/MainVerticle.kt
@@ -1,25 +1,91 @@
 package gov.nist.secauto.oscal.tools.server
 
-import io.vertx.core.AbstractVerticle
-import io.vertx.core.Promise
+import gov.nist.secauto.oscal.tools.cli.core.CLI
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
+import io.vertx.ext.web.openapi.OpenAPILoaderOptions
+import io.vertx.ext.web.openapi.RouterBuilder
+import io.vertx.core.json.JsonObject
+import io.vertx.kotlin.coroutines.CoroutineVerticle
+import io.vertx.kotlin.coroutines.coAwait
+import io.vertx.kotlin.coroutines.dispatcher
+import kotlinx.coroutines.launch
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 
-class MainVerticle : AbstractVerticle() {
+class MainVerticle : CoroutineVerticle() {
 
-  override fun start(startPromise: Promise<Void>) {
-    vertx
-      .createHttpServer()
-      .requestHandler { req ->
-        req.response()
-          .putHeader("content-type", "text/plain")
-          .end("Hello from Vert.x!")
-      }
-      .listen(8888).onComplete { http ->
-        if (http.succeeded()) {
-          startPromise.complete()
-          println("HTTP server started on port 8888")
-        } else {
-          startPromise.fail(http.cause());
+    companion object {
+        private val logger: Logger = LogManager.getLogger(MainVerticle::class.java)
+    }
+
+    override suspend fun start() {
+        val router = createRouter()
+        startHttpServer(router)
+    }
+
+    private suspend fun createRouter(): Router {
+        val options = OpenAPILoaderOptions()
+        val routerBuilder = RouterBuilder.create(vertx, "openapi.yaml", options).coAwait()
+        routerBuilder.operation("runCli").handler(::handleCliRequest)
+        return routerBuilder.createRouter()
+    }
+
+    private fun startHttpServer(router: Router) {
+        vertx.createHttpServer()
+            .requestHandler(router)
+            .listen(8888)
+            .onSuccess { 
+                logger.info("HTTP server started on port 8888")
+            }
+            .onFailure { error ->
+                logger.error("Failed to start HTTP server", error)
+            }
+    }
+
+    private fun handleCliRequest(ctx: RoutingContext) {
+        val command = ctx.queryParam("command").firstOrNull()
+        if (command.isNullOrEmpty()) {
+            sendErrorResponse(ctx, 400, "Missing 'command' query parameter")
+            return
         }
-      }
-  }
+
+        logger.info("Received CLI request with command: $command")
+
+        launch(vertx.dispatcher()) {
+            try {
+                val args = parseCommandToArgs(command)
+                val result = CLI.runCli(*args)
+                sendSuccessResponse(ctx, result.exitCode.statusCode, result.exitCode.name)
+            } catch (e: Exception) {
+                logger.error("Error executing CLI command", e)
+                sendErrorResponse(ctx, 500, "Internal Server Error")
+            }
+        }
+    }
+
+    private fun parseCommandToArgs(command: String): Array<String> {
+        return command.split("\\s+".toRegex())
+            .filter { it.isNotBlank() }
+            .toTypedArray()
+    }
+
+    private fun sendSuccessResponse(ctx: RoutingContext, exitCode: Int, status: String) {
+        val responseJson = JsonObject()
+            .put("exitCode", exitCode)
+            .put("status", status)
+
+        logger.info("CLI execution completed with exit code: $exitCode")
+
+        ctx.response()
+            .putHeader("content-type", "application/json")
+            .end(responseJson.encode())
+    }
+
+    private fun sendErrorResponse(ctx: RoutingContext, statusCode: Int, message: String) {
+        ctx.response()
+            .setStatusCode(statusCode)
+            .putHeader("content-type", "application/json")
+            .end(JsonObject().put("error", message).encode())
+    }
 }
--- src/test/kotlin/gov/nist/secauto/oscal/tools/server/TestMainVerticle.kt
+++ src/test/kotlin/gov/nist/secauto/oscal/tools/server/TestMainVerticle.kt
@@ -1,29 +1,64 @@
 package gov.nist.secauto.oscal.tools.server
 
+import io.vertx.core.Vertx
+import io.vertx.ext.web.client.WebClient
+import io.vertx.ext.web.client.WebClientOptions
 import io.vertx.ext.unit.TestContext
-import io.vertx.ext.unit.junit.RunTestOnContext
 import io.vertx.ext.unit.junit.VertxUnitRunner
+import org.junit.After
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(VertxUnitRunner::class)
 class TestMainVerticle {
 
-  @Rule
-  @JvmField
-  val rule = RunTestOnContext()
+    private lateinit var vertx: Vertx
+    private lateinit var webClient: WebClient
 
-  @Before
-  fun deploy_verticle(testContext: TestContext) {
-    val vertx = rule.vertx()
-    vertx.deployVerticle(MainVerticle()).onComplete(testContext.asyncAssertSuccess())
-  }
+    @Before
+    fun setUp(testContext: TestContext) {
+        vertx = Vertx.vertx()
+        webClient = WebClient.create(vertx, WebClientOptions().setDefaultPort(8888))
+        
+        val async = testContext.async()
+        vertx.deployVerticle(MainVerticle()) { ar ->
+            if (ar.succeeded()) {
+                async.complete()
+            } else {
+                testContext.fail(ar.cause())
+            }
+        }
+    }
 
-  @Test
-  fun verticle_deployed(testContext: TestContext) {
-    val async = testContext.async()
-    async.complete()
-  }
+    @After
+    fun tearDown(testContext: TestContext) {
+        vertx.close(testContext.asyncAssertSuccess())
+    }
+
+    @Test
+    fun test_run_cli_command(testContext: TestContext) {
+        val async = testContext.async()
+
+        webClient.get("/run-cli")
+            .addQueryParam("command", "metaschema metapath list-functions")
+            .send { ar ->
+                if (ar.succeeded()) {
+                    val response = ar.result()
+                    testContext.assertEquals(200, response.statusCode())
+
+                    val body = response.bodyAsJsonObject()
+                    testContext.assertNotNull(body)
+                    testContext.assertTrue(body.containsKey("exitCode"))
+                    testContext.assertTrue(body.containsKey("status"))
+
+                    val exitCode = body.getInteger("exitCode")
+                    testContext.assertEquals(0, exitCode, "Expected exit code 0, but got $exitCode")
+
+                    async.complete()
+                } else {
+                    testContext.fail(ar.cause())
+                }
+            }
+    }
 }
